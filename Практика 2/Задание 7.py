902af4631de8a40ee76f77e4ee8b44c67e8fe96b
Github40410 <indyukoffv42@mail.ru>
2021-09-29 17:19:21 +0300

    изменения

--- Практика 2/Задание 7.py
+++ Практика 2/Задание 7.py
@@ -2,7 +2,7 @@ def vus():
     ear=int(input())
     if ear%100==0:
         print("Нет")
-    if (ear%400==0) or (ear%4==0):
+    if (ear%400==0) and (ear%4==0):
         print("Да")
     else:
         print("Нет")
