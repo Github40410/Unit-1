4bfa0e5a8ee8b5019bc91b1f08b22b9ef713d989
Github40410 <indyukoffv42@mail.ru>
2022-02-22 14:04:26 +0300

    Лабораторные задания 1

---
+++ Лабораторная 1/Zd4.java
@@ -0,0 +1,15 @@
+public class Zd4 {
+public static void main(String N[])
+{
+String log="Vlad";
+String par="1234";
+if((N[0].equals(log))&(N[1].equals(par)))
+{
+System.out.println("Вас узнали. Добро пожаловать.");
+}
+else
+{
+System.out.println("Логин и пароль не распознаны. Доступ запрещён.");
+}
+}
+}
