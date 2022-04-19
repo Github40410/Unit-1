c06895860b54ed0a991c0aea9e33c93797e91559
Github40410 <indyukoffv42@mail.ru>
2022-04-19 21:40:32 +0300

    Добавил

--- Лабораторная 3/src/TestZd3.java
+++
@@ -1,9 +0,0 @@
-import Zd3.*;
-public class TestZd3 {
-    public static void main(String[] arg) {
-        Tourist tur = new Tourist(1234500, "Иванов", "Зелёный росток", 11, "курорт");
-        System.out.println(tur.toString());
-        System.out.println(tur);
-    }
-}
-
